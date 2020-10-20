5b9ddff568c0a9e2cf2e24165bcbe370382c8a80
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-19 21:28:30 -0400

    attempted to solve all solutions

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 // Your code below.
 
+const makeDino = function (name, period, carnivore, extinct) {
+  const newDino = {
+    species: name,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct,
+  }
+  if (extinct === undefined) {
+    newDino.extinct = false
+  }
+  return newDino
+}
 
+const makeExtinct = function (dino1) {
+  const deadDino = makeDino(dino1.species, dino1.period, dino1.carnivore, dino1.extinct);
+  deadDino.extinct = true;
 
+  return deadDino;
+}
 
+const truncateSpecies = function (name, period, carnivore, extinct) {
+  const shortNameDino = {
+    species:name,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct,
+  }
+  return shortNameDino   
+}
+
+/* So not sure how this truncate works.  I know we can .slice but not sure how to use that in 
+in a function.  Would I use an if statement, would I add another function.  This is where I get 
+stuck when you ask me to extract something, return something less, ugh feeling defeated yet again.
+Sorry this is far as I could get even with reading online articles.
+*/
+
+const makeSingular = function (name, period, carnivore, extinct) {
+  const noUsInName = {
+    species:name,
+    period: period,
+    carnivore: carnivore, 
+    extinct: extinct,
+  }
+  return noUsInName
+}
+
+//  Best I can do is give you a non-mutated object. 
 
 // Our code below; DO NOT TOUCH!
 if (typeof makeDino === 'undefined') {
